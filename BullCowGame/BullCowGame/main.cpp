f634ab8e2d37eb529ce3fefac81bf3d40ae80090
Jared E Jackson <jaredjxn15@gmail.com>
2018-08-04 22:54:39 -0400

    Initial Commit

---
+++ BullCowGame/BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Jared  Jackson on 8/4/18.
+//  Copyright © 2018 Jared  Jackson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
