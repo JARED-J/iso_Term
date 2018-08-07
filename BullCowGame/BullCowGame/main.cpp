56580f4525cfc898ae5bb322b42585314c51afd0
Jared E Jackson <jaredjxn15@gmail.com>
2018-08-07 17:36:33 -0400

    added player input/output loop

--- BullCowGame/BullCowGame/main.cpp
+++ BullCowGame/BullCowGame/main.cpp
@@ -1,15 +1,53 @@
-//
-//  main.cpp
-//  BullCowGame
-//
-//  Created by Jared  Jackson on 8/4/18.
-//  Copyright © 2018 Jared  Jackson. All rights reserved.
-//
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+void PrintIntro();
+string GetGuess();
+void PlayGame();
+bool AskToPlayAgain();
+
+// entry point for application
+int main() {
+    PrintIntro();
+    PlayGame();
+    AskToPlayAgain();
     return 0;
+}
+
+void PrintIntro() {
+    // introduce the game
+    constexpr int WORD_LENGTH = 9;
+    cout << "Welcome to Bulls and Cows, a fun word game.\n";
+    cout << "Can you guess the "<<WORD_LENGTH;
+    cout << " letter isogram I'm thinking of?\n";
+    cout << endl;
+    return;
+}
+
+string GetGuess() {
+    // get a guess from the player
+    cout << "Enter your guess: ";
+    string Guess = "";
+    getline(cin, Guess);
+    return Guess;
+}
+
+void PlayGame() {
+    // loop for the amount of guesses a player gets
+    constexpr int NUMBER_OF_TURNS = 5;
+    for (int count = 0; count < NUMBER_OF_TURNS; count++) {
+        string Guess = GetGuess();
+        cout << "Your Guess was: "<< Guess << endl;
+        cout << endl;
+    }
+}
+
+bool AskToPlayAgain(){
+    cout << "Do you want to play again?: ";
+    string Response = "";
+    getline(cin, Response);
+    return (Response[0] == 'y') || (Response[0] == 'Y');
 }
